e38052d60847719a3e8b69c5521ad2ccf8d68a04
JuaniSarmiento <juanisarmientoomartinez@gmail.com.com>
2025-10-04 16:12:54 -0300

    Solucionando Vite Config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,16 @@
 // En FRONTEND/vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-
-// ¡¡¡EL CAMBIO ESTÁ ACÁ!!!
-// Importamos las herramientas nuevas para manejar rutas en ES Modules.
-import { fileURLToPath, URL } from 'url'; 
+// Sacamos 'path' porque ya no se usa de la forma antigua.
+// import path from 'path'; 
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <--- ¡¡¡ESTA ES LA LÍNEA MÁGICA!!!
   resolve: {
     alias: {
-      // Usamos la nueva forma para decirle a Vite dónde está la carpeta 'src'.
-      // Esto es compatible con "type": "module".
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
